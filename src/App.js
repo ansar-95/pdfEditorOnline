5cc961f8e56793869bbc7aac25d1c436fe3d9244
Ansarr Ahamadou <moha.ans@hotmail.com>
2022-10-08 22:37:11 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/HeaderItem/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Helloo</h1>
+    <div>
+      <Header/>
     </div>
   );
 }
--- src/Components/HeaderItem/Header.js
+++ src/Components/HeaderItem/Header.js
@@ -1,10 +1,22 @@
 import './header.css'
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
     return (
       <header>
-        
+        <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">Editor Pdf</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                  <Nav.Link href="#home">Editor</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+    </Navbar>
       </header>
     );
   }
