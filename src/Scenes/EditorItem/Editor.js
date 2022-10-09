2f66fc27da80e7498eafac9e11cf4743ab3b19d6
Ansarr Ahamadou <moha.ans@hotmail.com>
2022-10-09 16:09:13 +0200

    add button

--- src/Scenes/EditorItem/Editor.js
+++ src/Scenes/EditorItem/Editor.js
@@ -1,8 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../../Components/HeaderItem/Header';
+import './editor.css'
+import Button from 'react-bootstrap/Button';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function Editor() {
   return (
-    <div></div>
+    <div>
+      <Header/>
+      <h1>Editor Pdf</h1>
+
+      <Container>
+          <Row className="justify-content-md-center mt-3">
+            <Col xs lg="2">
+            <Button variant="danger" className=''>Import  pdf</Button>
+            </Col>
+            
+          </Row>
+      </Container>  
+        
+    
+
+    </div>
   );
 }
 
